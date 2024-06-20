da098c5d51b2978299dfc94d2808c045ccc2234b
Mohamed Mousa <105562159+MohamedMousaa@users.noreply.github.com>
2024-06-20 19:24:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,46 +9,35 @@ st.subheader("Upload the image to analyze it")
 uploaded_file = st.file_uploader("Upload image", type=['png', 'jpg', 'jpeg'])
 
 if uploaded_file is not None:
-    # Load the image
     image = Image.open(uploaded_file)
     
-    # Display the uploaded image
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
     analyser = st.button('Analyse Image')
 
     if analyser:
-        # Load the model
         model = YOLO("best.pt")
 
-        # Run the model on the uploaded image
         results = model(image)
 
-        # Dictionary to store detected component names and their counts
         detected_counts = {}
 
         for result in results:
-            # Plot the results
             annotated_image = result.plot()  # This returns a numpy array
 
-            # Convert the numpy array to a PIL Image
             annotated_image = Image.fromarray(annotated_image)
 
-            # Display the annotated image
             st.image(annotated_image, caption='Annotated Image', use_column_width=True)
 
-            # Extract and display detected component names
             for box in result.boxes:
-                class_id = box.cls  # Get the class ID of the detected object
-                class_name = model.names[int(class_id)]  # Get the class name using the model's names attribute
+                class_id = box.cls
+                class_name = model.names[int(class_id)]
                 
-                # Update the count of the detected class name
                 if class_name in detected_counts:
                     detected_counts[class_name] += 1
                 else:
                     detected_counts[class_name] = 1
 
-        # Display the list of detected names and their counts
         if detected_counts:
             st.write("Detected components:")
             for name, count in detected_counts.items():
